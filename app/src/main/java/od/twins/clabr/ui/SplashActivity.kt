69a66769fc54c3fc575aaf2deb3f40ea117aa916
Vladymyr <twins88odessa@gmail.com>
2020-03-06 22:05:56 +0200

    Initial commit

---
+++ app/src/main/java/od/twins/clabr/ui/SplashActivity.kt
@@ -0,0 +1,14 @@
+package od.twins.clabr.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
