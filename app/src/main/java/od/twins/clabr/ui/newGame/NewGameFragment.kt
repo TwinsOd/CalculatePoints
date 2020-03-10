95641ce5c5e35269801ca80d33d3f027c7add7c8
Vladymyr <twins88odessa@gmail.com>
2020-03-10 17:15:28 +0200

    fixed new game ui

--- app/src/main/java/od/twins/clabr/ui/newGame/NewGameFragment.kt
+++ app/src/main/java/od/twins/clabr/ui/newGame/NewGameFragment.kt
@@ -7,9 +7,15 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.google.android.material.button.MaterialButtonToggleGroup
 import od.twins.clabr.R
+import od.twins.clabr.data.models.GameType
+import od.twins.clabr.data.models.LimitPoints
 
 class NewGameFragment : Fragment() {
+    val TAG = "NewGameFragment"
+    lateinit var gameType: GameType
+    lateinit var pointLimit: LimitPoints
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +28,30 @@ class NewGameFragment : Fragment() {
             Navigation.findNavController(v).navigate(R.id.action_newGameFragment_to_namesFragment)
         }
 
+        val typeGameView: MaterialButtonToggleGroup =
+            root.findViewById(R.id.type_game_toggle_button)
+        typeGameView.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked)
+                run {
+                    when (checkedId) {
+                        R.id.two_button -> gameType = GameType.TWO
+                        R.id.three_button -> gameType = GameType.THREE
+                        R.id.four_button -> gameType = GameType.FOUR
+                        R.id.pair_button -> gameType = GameType.TWO_ON_TWO
+                    }
+                }
+        }
+        val pointLimitView: MaterialButtonToggleGroup =
+            root.findViewById(R.id.point_limit_toggle_button)
+        pointLimitView.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked)
+                run {
+                    when (checkedId) {
+                        R.id.small_game -> pointLimit = LimitPoints.SMALL
+                        R.id.big_game -> pointLimit = LimitPoints.BIG
+                    }
+                }
+        }
         return root
     }
 }
