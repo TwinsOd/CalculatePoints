48d3057d496a21d5e00ccbaabcbbcec2c9ed203d
Vladymyr <twins88odessa@gmail.com>
2020-10-26 21:41:31 +0200

    refactoring NamesFragment

--- app/src/main/java/od/twins/clabr/ui/names/NamesFragment.kt
+++ app/src/main/java/od/twins/clabr/ui/names/NamesFragment.kt
@@ -10,9 +10,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.fragment_names.*
 import od.twins.clabr.R
 import od.twins.clabr.data.models.GameType
 import od.twins.clabr.data.models.LimitPoints
@@ -24,7 +22,7 @@ import od.twins.clabr.ui.newGame.ARG_POINT_LIMIT
 
 private const val TAG = "NamesFragment"
 
-class NamesFragment : Fragment() {
+class NamesFragment : Fragment(), View.OnFocusChangeListener {
     private var listener: OnListFragmentInteractionListener? = null
     private var gameType: GameType? = null
     private var pointLimit: LimitPoints? = null
@@ -34,9 +32,6 @@ class NamesFragment : Fragment() {
         arguments?.let {
             gameType = it.get(ARG_GAME_TYPE) as GameType
             pointLimit = it.get(ARG_POINT_LIMIT) as LimitPoints
-
-            Log.i(TAG, "onCreate gameType=$gameType");
-            Log.i(TAG, "onCreate pointLimit=$pointLimit");
         }
     }
 
@@ -44,19 +39,21 @@ class NamesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_names, container, false)
+        return inflater.inflate(R.layout.fragment_names, container, false)
+    }
 
-        val nameList: RecyclerView = root.findViewById(R.id.names_list)
-        nameList.apply {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        names_list.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = NameRecyclerViewAdapter(DummyContent.ITEMS, listener)
         }
 
-        val nextView: FloatingActionButton = root.findViewById(R.id.next_view)
-        nextView.setOnClickListener { v ->
+        next_view.setOnClickListener { v ->
             when (gameType) {
                 GameType.TWO, GameType.THREE, GameType.FOUR ->
-                    Toast.makeText(root.context, "In development", Toast.LENGTH_LONG).show()
+                    Toast.makeText(view.context, "In development", Toast.LENGTH_LONG).show()
                 GameType.TWO_ON_TWO -> {
                     Navigation.findNavController(v)
                         .navigate(R.id.action_namesFragment_to_twoTwoCalculateFragment)
@@ -64,20 +61,20 @@ class NamesFragment : Fragment() {
             }
         }
 
-        val player1Layout: TextInputLayout = root.findViewById(R.id.player_1)
-        val player2Layout: TextInputLayout = root.findViewById(R.id.player_2)
-        val player3Layout: TextInputLayout = root.findViewById(R.id.player_3)
-        val player4Layout: TextInputLayout = root.findViewById(R.id.player_4)
+        player_1.editText?.onFocusChangeListener = this
+        player_2.editText?.onFocusChangeListener = this
+//        val player3Layout: TextInputLayout = root.findViewById(R.id.player_3)
+//        val player4Layout: TextInputLayout = root.findViewById(R.id.player_4)
 //        val player1editText = TextInputEditText(player1Layout.context);
 
-        Log.i(TAG, "onCreateView gameType=$gameType");
+        Log.i(TAG, "onCreateView gameType=$gameType")
 
         when (gameType) {
             GameType.TWO -> {
-                player3Layout.visibility = GONE
-                player4Layout.visibility = GONE
+                player_3.visibility = GONE
+                player_4.visibility = GONE
             }
-            GameType.THREE -> player4Layout.visibility = GONE
+            GameType.THREE -> player_4.visibility = GONE
             GameType.TWO_ON_TWO -> {
                 //выделить по групам
 //                player1Layout.boxStrokeColor = resources.getColor(R.color.light_grey)
@@ -86,12 +83,20 @@ class NamesFragment : Fragment() {
 //                player4Layout.boxStrokeColor = resources.getColor(R.color.blue_grey)
             }
         }
-
-        return root
     }
 
     interface OnListFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onListFragmentInteraction(item: DummyItem?)
+    }
+
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+//        Log.i(TAG, "onFocusChange")
+//        Toast.makeText(v?.context, "onFocusChange", Toast.LENGTH_LONG).show()
+//        when(v?.id){
+//            R.id.player_1 -> player1Layout.editText?.setText("Misha")
+//            R.id.player_2 -> player1Layout.editText?.setText("YAN")
+//        }
+        Log.i(TAG, "onFocusChange")
     }
 }
